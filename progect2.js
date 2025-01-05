51226aab6a6ab462fe6cc296cf0e8d229a32b8b9
Litvin-13 <153170315+Litvin-13@users.noreply.github.com>
2025-01-05 21:36:17 +0300

    05.01 progect2.js

--- progect2.js
+++ progect2.js
@@ -351,7 +351,8 @@ function findVibro(eo) {
             for (let bollX = 1; bollX <= totalBolls; bollX++) {
                 
                 if (eoX>pointBolls[bollY][bollX]['x мяча']-bollRadius&&eoX<pointBolls[bollY][bollX]['x мяча']+bollRadius&&eoY>pointBolls[bollY][bollX]['y мяча']-bollRadius&&eoY<pointBolls[bollY][bollX]['y мяча']+bollRadius) {
-                    alert(`по Х ${eoX}, по Y ${eoY},${pointBolls[bollY][bollX]['x мяча']},${pointBolls[bollY][bollX]['y мяча']}`)
+                                        alert(`по Х ${eoX}, по Y ${eoY},${pointBolls[bollY][bollX]['x мяча']},${pointBolls[bollY][bollX]['y мяча']},${colorsBolls[bollY][bollX]},${stopColor}`)
+
                     window.navigator.vibrate(200)
                 }
             }
